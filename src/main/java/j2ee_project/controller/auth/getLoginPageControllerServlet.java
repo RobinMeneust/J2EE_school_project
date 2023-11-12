117dc23cae85a3dfcb5f0e8ab81eae0bfa21b61b
Lucas <lucas.velay2@gmail.com>
2023-11-12 16:03:37 +0100

    feat #7 : Add route for login and register pages

---
+++ src/main/java/j2ee_project/controller/auth/getLoginPageControllerServlet.java
@@ -0,0 +1,30 @@
+package j2ee_project.controller.auth;
+
+import j2ee_project.dao.catalog.product.ProductDAO;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(value = "/login")
+public class getLoginPageControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try {
+            RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/login.jsp");
+            view.forward(request, response);
+        } catch(Exception err) {
+            // The forward didn't work
+            System.err.println(err.getMessage());
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

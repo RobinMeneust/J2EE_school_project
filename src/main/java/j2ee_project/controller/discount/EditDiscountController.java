e85934b7d73da4131f422856e644c27fec9a49de
Jylen02 <saelenjere@cy-tech.fr>
2023-12-10 00:02:24 +0100

    end of edit discount.

---
+++ src/main/java/j2ee_project/controller/discount/EditDiscountController.java
@@ -0,0 +1,124 @@
+package j2ee_project.controller.discount;
+
+import j2ee_project.dao.discount.DiscountDAO;
+import j2ee_project.dto.discount.DiscountDTO;
+import j2ee_project.model.Discount;
+import j2ee_project.model.user.Moderator;
+import j2ee_project.model.user.TypePermission;
+import j2ee_project.service.DTOService;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Map;
+
+import static j2ee_project.dao.user.PermissionDAO.getPermission;
+
+@WebServlet("/edit-discount")
+public class EditDiscountController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            HttpSession session = request.getSession();
+            Object obj = session.getAttribute("user");
+            if (obj instanceof Moderator user
+                    && user.isAllowed(getPermission(TypePermission.CAN_MANAGE_DISCOUNT))) {
+                String discountIdStr = request.getParameter("id");
+                int discountId = -1;
+
+                if (discountIdStr != null && !discountIdStr.trim().isEmpty()) {
+                    try {
+                        discountId = Integer.parseInt(discountIdStr);
+                    } catch (Exception ignore) {
+                    }
+                }
+
+                if (discountId <= 0) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Discount ID must be positive");
+                }
+
+                Discount discount = DiscountDAO.getDiscount(discountId);
+                request.setAttribute("discount", discount);
+                RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/dashboard/edit/editDiscount.jsp");
+                view.forward(request, response);
+            } else {
+                response.sendRedirect("dashboard");
+            }
+        } catch(Exception err) {
+            System.err.println(err.getMessage());
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String discountIdStr = request.getParameter("id");
+        int discountId = -1;
+
+        if (discountIdStr != null && !discountIdStr.trim().isEmpty()) {
+            try {
+                discountId = Integer.parseInt(discountIdStr);
+            } catch (Exception ignore) {
+            }
+        }
+
+        if (discountId <= 0) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Discount ID must be positive");
+        }
+
+        Discount discount = DiscountDAO.getDiscount(discountId);
+
+        try {
+
+            DiscountDTO discountDTO = new DiscountDTO(
+                    request.getParameter("name"),
+                    Date.valueOf(request.getParameter("startDate")),
+                    Date.valueOf(request.getParameter("endDate")),
+                    Integer.parseInt(request.getParameter("discountPercentage"))
+            );
+
+            Map<String, String> inputErrors = DTOService.discountDataValidation(discountDTO);
+
+            String errorDestination = "WEB-INF/views/dashboard/edit/editDiscount.jsp";
+            RequestDispatcher dispatcher = null;
+
+            if (inputErrors.isEmpty()) {
+                try {
+
+                    if (discountDTO.getName() != null && !discountDTO.getName().isEmpty()) {
+                        discount.setName(discountDTO.getName());
+                    }
+                    if (discountDTO.getStartDate() != null) {
+                        discount.setStartDate(discountDTO.getStartDate());
+                    }
+                    if (discountDTO.getEndDate() != null) {
+                        discount.setEndDate(discountDTO.getEndDate());
+                    }
+                    discount.setDiscountPercentage(discountDTO.getDiscountPercentage());
+
+
+                    DiscountDAO.updateDiscount(discount);
+                    response.sendRedirect("dashboard?tab=discounts");
+                } catch (Exception exception) {
+                    System.err.println(exception.getMessage());
+                    request.setAttribute("ModificationProcessError", "Error during Modification process");
+                    dispatcher = request.getRequestDispatcher(errorDestination);
+                    dispatcher.include(request, response);
+                }
+            } else {
+                request.setAttribute("InputError", inputErrors);
+                dispatcher = request.getRequestDispatcher(errorDestination);
+                dispatcher.include(request, response);
+            }
+
+            if (dispatcher != null) doGet(request, response);
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+}
--- src/main/java/j2ee_project/dto/discount/DiscountDTO.java
+++ src/main/java/j2ee_project/dto/discount/DiscountDTO.java
@@ -12,7 +12,7 @@ import java.sql.Date;
 public class DiscountDTO {
     @NotBlank(message = "Name can not be blank.")
     @Size(max = 30, message = "Name can not exceed 30 characters.")
-    @Pattern(regexp = "^[a-zA-ZÀ-ÖØ-öø-ÿ\\-']*$", message = "Name is not valid : only letters and -' are authorized.")
+    @Pattern(regexp = "^[a-zA-ZÀ-ÖØ-öø-ÿ\\-' ]*$", message = "Name is not valid : only letters and -' are authorized.")
     private String name;
     @NotBlank(message = "Start date can not be blank.")
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Start date is not valid.")
